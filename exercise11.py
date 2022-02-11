98d2f77c2487cb77b634d0bc394a4df4f8123474
Jody Broad <msjodybroad@gmail.com>
2022-02-11 10:49:33 +0000

    finished question 3

--- exercise11.py
+++ exercise11.py
@@ -13,3 +13,18 @@ print(Belgium.replace(',', ':'))
 
 # followed by the population of Belgium (the second field) plus the population of the capital city (the fourth field)
 # Hint: the answer should be 11183818.
+
+# belgium_population = Belgium[8:16]
+# print(belgium_population)
+# brussels_population = Belgium[27:32]
+# print(brussels_population)
+# total_population = int(belgium_population) + int(brussels_population)
+# print(total_population)
+
+# split is turning one long string into a list, and seperating with the ,
+belgium_list = Belgium.split(',')
+print(belgium_list)
+# use int to convert to integer to add
+print(int(belgium_list[1]) + int(belgium_list[3]))
+
+
