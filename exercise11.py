7260c8990c18f4eb562aac9e3c43997803eb8077
Jeje <jehan.alfarra@gmail.com>
2022-02-11 13:36:18 +0300

    Q1 & Q2 of Exercise 11 done

--- exercise11.py
+++ exercise11.py
@@ -4,7 +4,12 @@ Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,F
 
 # add code to print a line of hyphens the same length as the Belgium string
 
+length_of_Belgium = len(Belgium)
+print(length_of_Belgium)
+print(length_of_Belgium * '-')
+
 # followed by the string with the comma separators replaced by colons ':'
+print(Belgium.replace(',', ':'))
 
 # followed by the population of Belgium (the second field) plus the population of the capital city (the fourth field)
 # Hint: the answer should be 11183818.
